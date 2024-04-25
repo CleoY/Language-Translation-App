0fa87adaf669130678eaef4a50930413253e0f33
CleoY <77142560+CleoY@users.noreply.github.com>
2024-04-25 00:29:09 -0700

    Tested routing w/ a variable.

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ client = OpenAI()
 def hello():
   return "Hi"
 
-# user_input = '''
-# Review "我很恨喜歡蘋果。"
-# '''
+user_input = '''
+Review "我很恨喜歡蘋果。"
+'''
 
-@app.route("/translate/")
+@app.route("/translate/<user_input>")
 # def translate(user_input):
-def translate():
+def translate(user_input):
   translationSetup = '''
   For all of the following commands, please give results in the specified JSON formats.
   When I type "Translate x to English", I want you to translate x into English, giving me the pinyin, English definition, and 1 example sentence in traditional Chinese with English translations. JSON list: pinyin, English_definition, exSentenceChinese, sentencePinyin, translation.
   When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, giving me the characters, pinyin, synonyms, and 1 example sentence for the Chinese word with English translations. JSON list: characters, pinyin, synonyms, exSentenceChinese, sentencePinyin, translation.
   When I type "Review x", I want you to make any necessary corrections to my grammar and spelling, or even give me suggestions for a better way to say what I mean. JSON list: original, corrected, suggestionNotes.
   '''
-  user_input = '''
-    Review "我很恨喜歡蘋果。"
-    '''
+#   user_input = '''
+#     Review "我很恨喜歡蘋果。"
+#     '''
 
   chat_completion = client.chat.completions.create(
       messages=[{
@@ -42,5 +42,5 @@ def translate():
 
 
 
-app.run()
+app.run(debug=True)
 # app.run(host="0.0.0.0", port=8000)
