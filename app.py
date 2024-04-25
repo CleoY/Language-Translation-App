6e951026f20cbf2d870f26d3973754ffc215118f
CleoY <77142560+CleoY@users.noreply.github.com>
2024-04-25 00:04:47 -0700

    Launching the flask server works so far. Just need a way to input user prompt.

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from openai import OpenAI
+from flask import Flask
+
+app = Flask(__name__)
 client = OpenAI()
 
+@app.route("/hello/")
+def hello():
+  return "Hi"
 
-def translate(user_input):
+# user_input = '''
+# Review "我很恨喜歡蘋果。"
+# '''
+
+@app.route("/translate/")
+# def translate(user_input):
+def translate():
   translationSetup = '''
   For all of the following commands, please give results in the specified JSON formats.
   When I type "Translate x to English", I want you to translate x into English, giving me the pinyin, English definition, and 1 example sentence in traditional Chinese with English translations. JSON list: pinyin, English_definition, exSentenceChinese, sentencePinyin, translation.
   When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, giving me the characters, pinyin, synonyms, and 1 example sentence for the Chinese word with English translations. JSON list: characters, pinyin, synonyms, exSentenceChinese, sentencePinyin, translation.
   When I type "Review x", I want you to make any necessary corrections to my grammar and spelling, or even give me suggestions for a better way to say what I mean. JSON list: original, corrected, suggestionNotes.
   '''
+  user_input = '''
+    Review "我很恨喜歡蘋果。"
+    '''
 
   chat_completion = client.chat.completions.create(
       messages=[{
@@ -26,7 +41,6 @@ def translate(user_input):
   return result
 
 
-user_input = '''
-Review "我很恨喜歡蘋果。"
-'''
-translate(user_input)
+
+
+app.run(host="0.0.0.0", port=8000)
