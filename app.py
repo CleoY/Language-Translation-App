326600fb597e3017362aec543a6c4658bf9b5d71
CleoY <77142560+CleoY@users.noreply.github.com>
2024-04-28 23:32:48 -0700

    Everything seems to work now. Renamed results.html

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
 #@app.route('/translate', methods=['POST'])
 def translate(prompt, keyword):
   translationSetup = '''
-  For the following command, please give results in the specified JSON formats.
+  For the following command, please give results in the specified JSON formats. Any and all Chinese should be traditional characters.
   ''' + prompt
   user_input = request.form['user_input']
   # You can use the user_input here to send it to the GPT-3 model
@@ -38,14 +38,14 @@ def translate(prompt, keyword):
 def translateChineseToEnglish():
    prompt = '''When I type "Translate x to English", I want you to translate x into English, giving me the pinyin, English definition, and 1 example sentence in traditional Chinese with English translations. JSON list: characters, pinyin, English_definition, exSentenceChinese, sentencePinyin, translation.'''
    result, user_input = translate(prompt, "Translate")
-   return render_template("results.html", user_input=user_input, chat_response=result, ChineseToEnglish = True)
+   return render_template("translate.html", user_input=user_input, chat_response=result, ChineseToEnglish = True)
 
 
 @app.route('/EnglishToChinese', methods = ['POST'])
 def translateEnglishToChinese():
-   prompt = '''When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, repeating the original English word, giving me the characters, pinyin, synonyms, and 1 example sentence for the Chinese word with English translations. JSON list: original, characters, pinyin, synonyms, synonymPinyin, exSentenceChinese, sentencePinyin, translation.'''
+   prompt = '''When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, repeating the original English word, giving me the characters, pinyin, synonyms, synonym translations, and 1 example sentence for the Chinese word with English translations. JSON list: original, characters, pinyin, synonyms, synonymTranslations, synonymPinyin, exSentenceChinese, sentencePinyin, translation.'''
    result, user_input = translate(prompt, "Translate")
-   return render_template("results.html", user_input=user_input, chat_response=result, ChineseToEnglish = False)
+   return render_template("translate.html", user_input=user_input, chat_response=result, ChineseToEnglish = False)
 
 
 @app.route('/review', methods = ['POST'])
@@ -55,96 +55,5 @@ def review():
   return render_template("review.html", user_input=user_input, chat_response=result)
 
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-# from openai import OpenAI
-# from flask import Flask, render_template, request
-
-# app = Flask(__name__)
-# client = OpenAI()
-
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
-
-# # @app.route("/hello/")
-# # def hello():
-# #   return "Hi"
-
-# user_input = '''
-# Review "我很恨喜歡蘋果。"
-# '''
-
-# @app.route('/')
-# def homePage():
-#    return render_template('home.html')
-
-
-# @app.route('/go', methods=['POST'])
-# def chat():
-#   user_input = request.form['user_input']
-#   # You can use the user_input here to send it to the GPT-3 model
-#   translationSetup = '''
-#   For all of the following commands, please give results in the specified JSON formats.
-#   When I type "Translate x to English", I want you to translate x into English, giving me the pinyin, English definition, and 1 example sentence in traditional Chinese with English translations. JSON list: pinyin, English_definition, exSentenceChinese, sentencePinyin, translation.
-#   When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, giving me the characters, pinyin, synonyms, and 1 example sentence for the Chinese word with English translations. JSON list: characters, pinyin, synonyms, exSentenceChinese, sentencePinyin, translation.
-#   When I type "Review x", I want you to make any necessary corrections to my grammar and spelling, or even give me suggestions for a better way to say what I mean. JSON list: original, corrected, suggestionNotes.
-#   '''
-  
-#   chat_completion = client.chat.completions.create(
-#     messages=[{
-#         "role": "system",
-#         "content": translationSetup
-#     }, {
-#         "role": "assistant",
-#         "content": user_input
-#     }],
-#     response_format={"type": "json_object"},
-#     model="gpt-4-1106-preview"
-#   )
-
-#   result = chat_completion.choices[0].message.content
-  
-#   # chat_response = response.choices[0].text.strip()
-#   return render_template('home.html', user_input=user_input, chat_response=result)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# # @app.route("/results/<user_input>")
-# # # def translate(user_input):
-# # def translate(user_input):
-# #   translationSetup = '''
-# #   For all of the following commands, please give results in the specified JSON formats.
-# #   When I type "Translate x to English", I want you to translate x into English, giving me the pinyin, English definition, and 1 example sentence in traditional Chinese with English translations. JSON list: pinyin, English_definition, exSentenceChinese, sentencePinyin, translation.
-# #   When I type "Translate x to Chinese", I want you to translate x into traditional Chinese, giving me the characters, pinyin, synonyms, and 1 example sentence for the Chinese word with English translations. JSON list: characters, pinyin, synonyms, exSentenceChinese, sentencePinyin, translation.
-# #   When I type "Review x", I want you to make any necessary corrections to my grammar and spelling, or even give me suggestions for a better way to say what I mean. JSON list: original, corrected, suggestionNotes.
-# #   '''
-# # #   user_input = '''
-# # #     Review "我很恨喜歡蘋果。"
-# # #     '''
-
-# #   chat_completion = client.chat.completions.create(
-# #       messages=[{
-# #           "role": "system",
-# #           "content": translationSetup
-# #       }, {
-# #           "role": "assistant",
-# #           "content": user_input
-# #       }],
-# #       response_format={"type": "json_object"},
-# #       model="gpt-4-1106-preview")
-
-# #   result = chat_completion.choices[0].message.content
-# #   print(result)
-# #   return result
-
-
-
-# # app.run(debug=True)
-# # # app.run(host="0.0.0.0", port=8000)
